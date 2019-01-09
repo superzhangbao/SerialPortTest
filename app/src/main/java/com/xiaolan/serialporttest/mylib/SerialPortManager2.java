2094c9f55dc70bcf46d3e1b03336083103575a0b
zhangbao <15056006309@163.com>
2019-01-09 15:19:47 +0800

    完成将报文写入文件的功能

--- app/src/main/java/com/xiaolan/serialporttest/mylib/SerialPortManager2.java
+++ app/src/main/java/com/xiaolan/serialporttest/mylib/SerialPortManager2.java
@@ -29,6 +29,10 @@ import io.reactivex.schedulers.Schedulers;
 /**
  * 测试push
  */
+
+/**
+ * 测试github push
+ */
 public class SerialPortManager2 {
     private static final String TAG = "SerialPortManager2";
     private SerialPort mSerialPort;
